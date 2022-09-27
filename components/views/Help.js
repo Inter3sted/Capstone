f35aa10f073097192f6b0ca1f27b569fb5a18eaa
Kolebe Shanks <skolebe@yahoo.com>
2022-09-26 20:04:54 -0500

    Dependencies installed and pseudo glass morphism

--- components/views/Help.js
+++ components/views/Help.js
@@ -1,6 +1,24 @@
 import html from "html-literal";
 
 export default (state) => html `
+<h1 class="faq">FAQ</h1>
+<h3>Question 1</h3>
+<p class="faqp">lorem</p>
+<h3>Question 2</h3>
+<p class="faqp">lorem</p>
+<h3>Question 3</h3>
+<p class="faqp">lorem</p>
+<div class="dropdown">
+    <button class="dropbtn">Question 1
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+<h3>Still have issues? Contact us:</h3>
 <p id="Help">
   <form action="https://formspree.io/f/xjvzwbbp" method="POST">
         <label>
--- components/views/Services.js
+++ components/views/Services.js
@@ -1,4 +1,30 @@
 import html from "html-literal";
 
 export default (state) => html `<section id="Services">
-</section>`;
+</section>
+<div class="glass">
+  <div class="cards">
+  <img src="https://images.pexels.com/photos/4771595/pexels-photo-4771595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="green landscape image">
+    <div class="therein">
+
+    <h2>Small</h2>
+    <a href="#">Read More</a>
+  </div></div>
+
+    <div class = "cards">
+    <img src="https://images.pexels.com/photos/4771595/pexels-photo-4771595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="green landscape image">
+      <div class = "therein">
+      <h2>Medium</h2>
+      <a href="#">Read More</a>
+    </div></div>
+
+    <div class = "cards">
+    <img src="https://images.pexels.com/photos/4771595/pexels-photo-4771595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="green landscape image">
+      <div class = "therein">
+      <h2>Large</h2>
+      <a href="#">Read More</a>
+    </div></div>
+  </div>
+    </div>
+
+    </div>`;
