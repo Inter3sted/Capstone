4718a20709c99a5608ec9b98e81d56eab1c8015a
Kolebe Shanks <skolebe@yahoo.com>
2022-10-06 18:33:56 -0500

    testing img

--- components/views/Map.js
+++ components/views/Map.js
@@ -4,4 +4,10 @@ export default (state) => html `  <section id="Map">
 <h2>Google Map Api</h2>
 <img src="https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="me">
 <p>Implement map api</p>
-</section>`;
+</section>
+<!-- Figure out how to make the nav bar do a similar thing -->
+<!-- https://www.eatthis.com/what-happens-body-drink-tea-every-day/ -->`;
+
+// index 0 is array within array, pass
+// index 1 caption
+// for popup, in bind
