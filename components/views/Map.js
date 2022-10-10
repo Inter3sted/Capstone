7775028347dcd47cd3abfdc2258490d76e35765c
Kolebe Shanks <skolebe@yahoo.com>
2022-10-09 21:34:54 -0500

    map implementation trials

--- components/views/Map.js
+++ components/views/Map.js
@@ -1,11 +1,11 @@
 import html from "html-literal";
 
-export default (state) => html `  <section id="Map">
+export default (state) => html `
 <h2>Map Api</h2>
 <div id="map"></div>
 </section>
 <h3>
-    ${state.weather.city} is experiencing ${state.weather.description}. Temperature is ${state.weather.temp}F, and the wind speed is ${state.weather.speed} mph.
+    ${state.weather.city} | ${state.weather.description}. Temperature | ${state.weather.temp}F. Wind speed | ${state.weather.speed} mph.
   </h3>
 <!-- Figure out how to make the nav bar do a similar thing -->
 <!-- https://www.eatthis.com/what-happens-body-drink-tea-every-day/ -->`;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
 import dotenv from "dotenv"
-
+import item from "./assets/pexels-spencer-davis-4771595.jpg"
 
 dotenv.config();
 
@@ -23,11 +23,84 @@ function render(state = store.Home) {
 }
 
 // add menu toggle to bars icon in nav bar
-function afterRender(state) {
-    document.querySelector(".fa-bars").addEventListener("click", () => {
-        document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-    });
-}
+async function afterRender(state) {
+    if (state.view === "Map") {
+        let mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+        mapboxgl.accessToken = `${process.env.MAPBOX_TOKEN}`;
+        const map = new mapboxgl.Map({
+            container: `map`,
+            style: `mapbox://styles/mapbox/streets-v11`,
+            center: [-86.78266047511858, 36.16525000567188],
+            zoom: 10,
+            projection: "globe"
+        });
+        map.on("load", () => {
+            // Load an image from an external URL.
+            map.loadImage(`${item}`, (error, image) => {
+                if (error) throw error;
+
+                // Add the image to the map style.
+                map.addImage("case", image);
+
+                // Add a data source containing one point feature.
+                map.addSource("places", {
+                    type: "geojson",
+                    data: {
+                        type: "FeatureCollection",
+                        features: state.cases
+                    }
+                });
+
+                map.addLayer({
+                    id: "places",
+                    type: "circle",
+                    source: "places",
+                    paint: {
+                        "circle-color": "#4264fb",
+                        "circle-radius": 6,
+                        "circle-stroke-width": 2,
+                        "circle-stroke-color": "#ffffff"
+                    }
+                });
+            });
+            // Create a popup, but don't add it to the map yet.
+            const popup = new mapboxgl.Popup({
+                closeButton: false,
+                closeOnClick: false
+            });
+
+            map.on("mouseenter", "places", e => {
+                // Change the cursor style as a UI indicator.
+                map.getCanvas().style.cursor = "pointer";
+
+                // Copy coordinates array.
+                const coordinates = e.features[0].geometry.coordinates.slice();
+                const description = e.features[0].properties.description;
+
+                // Ensure that if the map is zoomed out such that multiple
+                // copies of the feature are visible, the popup appears
+                // over the copy being pointed to.
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                }
+
+                // Populate the popup and set its coordinates
+                // based on the feature found.
+                popup
+                    .setLngLat(coordinates)
+                    .setHTML(description)
+                    .addTo(map);
+            });
+
+            map.on("mouseleave", "places", () => {
+                map.getCanvas().style.cursor = "";
+                popup.remove();
+            });
+        });
+    };
+};
+
 
 router.hooks({
     before: (done, params) => {
@@ -49,6 +122,7 @@ router.hooks({
                         store.Map.weather.speed = response.data.list[0].speed;
                         console.log(response.data);
                         done();
+
                     })
                     .catch((err) => {
                         console.log(err);
