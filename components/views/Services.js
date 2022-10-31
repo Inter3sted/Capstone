7a2f9b0fa3a06a7646228794df967b79dfe6aa52
Kolebe Shanks <skolebe@yahoo.com>
2022-10-30 23:27:25 -0500

    assets issues all dro import, potential issues with droC

--- components/views/Services.js
+++ components/views/Services.js
@@ -1,12 +1,12 @@
 import html from "html-literal";
 import droA from "../../assets/pexels-tembela-bohle.jpg";
 import droB from "../../assets/pexels-brett-saylesbw.jpg";
-
+import droC from "../../assets/pexels-maël-balland.jpg";
 export default () => html `
 
 <div class="glass">
   <div class="cards">
-
+  <img src='${droC}' alt="image of a drone on a black case with an overhead light" class="dark">
     <div class="therein">
     <h2>Small</h2>
     <p>$4</p>
