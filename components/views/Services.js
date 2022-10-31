f5f3df5bf4d43444dd2e2fc30d07299dc0cdef05
Kolebe Shanks <skolebe@yahoo.com>
2022-10-30 23:24:23 -0500

    assets issues droA & droB import

--- components/views/Services.js
+++ components/views/Services.js
@@ -1,5 +1,6 @@
 import html from "html-literal";
 import droA from "../../assets/pexels-tembela-bohle.jpg";
+import droB from "../../assets/pexels-brett-saylesbw.jpg";
 
 export default () => html `
 
@@ -12,7 +13,7 @@ export default () => html `
   </div></div>
 
   <div class="cards">
-
+    <img src='${droB}' alt="A drone in the distance flying over trees with clouds in the background" class="dark">
       <div class = "therein">
       <h2>Large</h2>
       <p>$6</p>
